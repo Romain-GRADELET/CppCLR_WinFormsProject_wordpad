c000cbfba7c47e49cbed2430fa58b3cbcdfcee3b
Romain-GRADELET <romain.gradelet73@outlook.fr>
2024-01-26 16:11:18 +0100

    ajout button pour agrandire la police

--- Form1.h
+++ Form1.h
@@ -34,6 +34,18 @@ namespace CppCLRWinFormsProject {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::MenuStrip^ menuStrip1;
+	protected:
+	private: System::Windows::Forms::ToolStripMenuItem^ fichierToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^ quitterToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^ aideToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^ toolStripMenuItem1;
+	private: System::Windows::Forms::ToolStrip^ toolStrip1;
+	private: System::Windows::Forms::ToolStripButton^ toolStripButton1;
+	private: System::Windows::Forms::ToolStripButton^ toolStripButton2;
+	private: System::Windows::Forms::ToolStripButton^ toolStripButton3;
+	private: System::Windows::Forms::RichTextBox^ richTextBox1;
+	private: System::Windows::Forms::ToolStripButton^ toolStripButton4;
 
 	private:
 		/// <summary>
@@ -48,12 +60,149 @@ namespace CppCLRWinFormsProject {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"Form1";
-			this->Padding = System::Windows::Forms::Padding(0);
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(Form1::typeid));
+			this->menuStrip1 = (gcnew System::Windows::Forms::MenuStrip());
+			this->fichierToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->quitterToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->aideToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->toolStripMenuItem1 = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->toolStrip1 = (gcnew System::Windows::Forms::ToolStrip());
+			this->toolStripButton1 = (gcnew System::Windows::Forms::ToolStripButton());
+			this->toolStripButton2 = (gcnew System::Windows::Forms::ToolStripButton());
+			this->toolStripButton3 = (gcnew System::Windows::Forms::ToolStripButton());
+			this->richTextBox1 = (gcnew System::Windows::Forms::RichTextBox());
+			this->toolStripButton4 = (gcnew System::Windows::Forms::ToolStripButton());
+			this->menuStrip1->SuspendLayout();
+			this->toolStrip1->SuspendLayout();
+			this->SuspendLayout();
+			// 
+			// menuStrip1
+			// 
+			this->menuStrip1->Items->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(3) {
+				this->fichierToolStripMenuItem,
+					this->aideToolStripMenuItem, this->toolStripMenuItem1
+			});
+			this->menuStrip1->Location = System::Drawing::Point(0, 0);
+			this->menuStrip1->Name = L"menuStrip1";
+			this->menuStrip1->Size = System::Drawing::Size(591, 24);
+			this->menuStrip1->TabIndex = 0;
+			this->menuStrip1->Text = L"menuStrip1";
+			// 
+			// fichierToolStripMenuItem
+			// 
+			this->fichierToolStripMenuItem->DropDownItems->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(1) { this->quitterToolStripMenuItem });
+			this->fichierToolStripMenuItem->Name = L"fichierToolStripMenuItem";
+			this->fichierToolStripMenuItem->Size = System::Drawing::Size(54, 20);
+			this->fichierToolStripMenuItem->Text = L"Fichier";
+			// 
+			// quitterToolStripMenuItem
+			// 
+			this->quitterToolStripMenuItem->Name = L"quitterToolStripMenuItem";
+			this->quitterToolStripMenuItem->Size = System::Drawing::Size(111, 22);
+			this->quitterToolStripMenuItem->Text = L"Quitter";
+			// 
+			// aideToolStripMenuItem
+			// 
+			this->aideToolStripMenuItem->Name = L"aideToolStripMenuItem";
+			this->aideToolStripMenuItem->Size = System::Drawing::Size(43, 20);
+			this->aideToolStripMenuItem->Text = L"Aide";
+			// 
+			// toolStripMenuItem1
+			// 
+			this->toolStripMenuItem1->Name = L"toolStripMenuItem1";
+			this->toolStripMenuItem1->Size = System::Drawing::Size(24, 20);
+			this->toolStripMenuItem1->Text = L"\?";
+			// 
+			// toolStrip1
+			// 
+			this->toolStrip1->Items->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(4) {
+				this->toolStripButton1,
+					this->toolStripButton2, this->toolStripButton3, this->toolStripButton4
+			});
+			this->toolStrip1->Location = System::Drawing::Point(0, 24);
+			this->toolStrip1->Name = L"toolStrip1";
+			this->toolStrip1->Size = System::Drawing::Size(591, 25);
+			this->toolStrip1->TabIndex = 1;
+			this->toolStrip1->Text = L"toolStrip1";
+			this->toolStrip1->ItemClicked += gcnew System::Windows::Forms::ToolStripItemClickedEventHandler(this, &Form1::toolStrip1_ItemClicked);
+			// 
+			// toolStripButton1
+			// 
+			this->toolStripButton1->DisplayStyle = System::Windows::Forms::ToolStripItemDisplayStyle::Text;
+			this->toolStripButton1->Name = L"toolStripButton1";
+			this->toolStripButton1->Size = System::Drawing::Size(35, 22);
+			this->toolStripButton1->Text = L"Bold";
+			// 
+			// toolStripButton2
+			// 
+			this->toolStripButton2->DisplayStyle = System::Windows::Forms::ToolStripItemDisplayStyle::Text;
+			this->toolStripButton2->Name = L"toolStripButton2";
+			this->toolStripButton2->Size = System::Drawing::Size(62, 22);
+			this->toolStripButton2->Text = L"Underline";
+			// 
+			// toolStripButton3
+			// 
+			this->toolStripButton3->DisplayStyle = System::Windows::Forms::ToolStripItemDisplayStyle::Text;
+			this->toolStripButton3->Name = L"toolStripButton3";
+			this->toolStripButton3->Size = System::Drawing::Size(46, 22);
+			this->toolStripButton3->Text = L"Center";
+			// 
+			// richTextBox1
+			// 
+			this->richTextBox1->Location = System::Drawing::Point(12, 52);
+			this->richTextBox1->Name = L"richTextBox1";
+			this->richTextBox1->Size = System::Drawing::Size(567, 291);
+			this->richTextBox1->TabIndex = 2;
+			this->richTextBox1->Text = L"";
+			// 
+			// toolStripButton4
+			// 
+			this->toolStripButton4->DisplayStyle = System::Windows::Forms::ToolStripItemDisplayStyle::Text;
+			this->toolStripButton4->Name = L"toolStripButton4";
+			this->toolStripButton4->Size = System::Drawing::Size(28, 22);
+			this->toolStripButton4->Text = L"Big";
+			// 
+			// Form1
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(591, 355);
+			this->Controls->Add(this->richTextBox1);
+			this->Controls->Add(this->toolStrip1);
+			this->Controls->Add(this->menuStrip1);
+			this->MainMenuStrip = this->menuStrip1;
+			this->Name = L"Form1";
+			this->Text = L"Form1";
+			this->menuStrip1->ResumeLayout(false);
+			this->menuStrip1->PerformLayout();
+			this->toolStrip1->ResumeLayout(false);
+			this->toolStrip1->PerformLayout();
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
 		}
 #pragma endregion
-	};
+
+	private: System::Void toolStrip1_ItemClicked(System::Object^ sender, System::Windows::Forms::ToolStripItemClickedEventArgs^ e) {
+		if (e->ClickedItem->Text == "Bold")
+		{
+			this->richTextBox1->SelectionFont = gcnew System::Drawing::Font(this->richTextBox1->Font, System::Drawing::FontStyle::Bold);
+		}
+		if (e->ClickedItem->Text == "Underline")
+		{
+			this->richTextBox1->SelectionFont = gcnew System::Drawing::Font(this->richTextBox1->Font, System::Drawing::FontStyle::Underline);
+
+		}
+		if (e->ClickedItem->Text == "Center")
+		{
+			this->richTextBox1->SelectionAlignment = System::Windows::Forms::HorizontalAlignment::Center;
+
+		}
+		if (e->ClickedItem->Text == "Big")
+		{
+			this->richTextBox1->SelectionFont = gcnew System::Drawing::Font(this->richTextBox1->Font->FontFamily, 16);
+
+		}
+	}
+};
 }
